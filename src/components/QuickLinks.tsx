47b5e7ee800c60799c3641346e8f074137c8fb70
Hiếu Lại <promex0401@gmail.com>
2025-07-22 18:14:14 +0000

    sao tôi thêm Facebook mà nó ko cập nhật đúng icon facebook v

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -97,13 +97,13 @@ export function QuickLinks() {
     setIsDialogOpen(false);
   };
   
-  const getIconForLink = (name: string) => {
-    const lowerName = name.toLowerCase();
+  const getIconForLink = (link: QuickLink) => {
+    const lowerName = link.name.toLowerCase();
     if (lowerName.includes('github')) {
         return (
           <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6">
             <title>GitHub</title>
-            <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+            <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
           </svg>
         )
     }
@@ -131,7 +131,12 @@ export function QuickLinks() {
           </svg>
         )
     }
-    return <img src={`https://www.google.com/s2/favicons?domain=${name}&sz=32`} alt={`${name} favicon`} className="h-6 w-6"/>
+    try {
+      const { hostname } = new URL(link.url);
+      return <img src={`https://www.google.com/s2/favicons?domain=${hostname}&sz=32`} alt={`${link.name} favicon`} className="h-6 w-6"/>
+    } catch (e) {
+      return <img src={`https://www.google.com/s2/favicons?domain=example.com&sz=32`} alt={`${link.name} favicon`} className="h-6 w-6"/>
+    }
   };
 
   return (
@@ -145,7 +150,7 @@ export function QuickLinks() {
             className="flex flex-col items-center gap-2 p-2 rounded-lg transition-colors hover:bg-secondary w-24"
             >
             <div className="p-3 rounded-full">
-                {getIconForLink(link.name)}
+                {getIconForLink(link)}
             </div>
             <span className="text-sm font-medium truncate w-full text-center">{link.name}</span>
             </a>
@@ -222,5 +227,3 @@ export function QuickLinks() {
     </div>
   );
 }
-
-    
