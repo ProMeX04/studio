bfcd68cf28dd8ecc0caf7faa0b16d9ec887b4af5
Hiếu Lại <promex0401@gmail.com>
2025-07-22 18:05:38 +0000

    Make the following changes:
    On the page src/app/page.tsx,
    
    Find this el

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -101,7 +101,7 @@ export function QuickLinks() {
     const lowerName = name.toLowerCase();
     if (lowerName.includes('github')) {
         return (
-          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-foreground">
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current">
             <title>GitHub</title>
             <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
           </svg>
@@ -109,7 +109,7 @@ export function QuickLinks() {
     }
     if (lowerName.includes('youtube')) {
         return (
-          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-foreground">
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current">
             <title>YouTube</title>
             <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
           </svg>
@@ -117,7 +117,7 @@ export function QuickLinks() {
     }
     if (lowerName.includes('gmail')) {
         return (
-          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-foreground">
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current">
             <title>Gmail</title>
             <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.71L12 16.64l-6.545-4.93v9.292H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.908 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/>
           </svg>
@@ -125,7 +125,7 @@ export function QuickLinks() {
     }
     if (lowerName.includes('firebase')) {
         return (
-          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-foreground">
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current">
             <title>Firebase</title>
             <path d="M4.213 15.548 3.03 5.41a.208.208 0 0 1 .25-.233l6.572 1.571a.208.208 0 0 1 .157.304l-3.32 5.419 2.286-1.32a.208.208 0 0 1 .26.039l2.747 3.42a.208.208 0 0 1-.165.341l-6.93-.456a.208.208 0 0 1-.19-.275zm15.424-7.235-8.54 15.65a.208.208 0 0 1-.366.01L7.1 18.067l7.55 4.36a.208.208 0 0 0 .285-.173L20.2 3.992a.208.208 0 0 0-.363-.279zM3.44.331.024 12.83a.208.208 0 0 0 .278.221L14.47 7.57a.208.208 0 0 0 .088-.35L3.841.34a.208.208 0 0 0-.401-.009z"/>
           </svg>
@@ -144,7 +144,7 @@ export function QuickLinks() {
             rel="noopener noreferrer"
             className="flex flex-col items-center gap-2 p-2 rounded-lg transition-colors hover:bg-secondary w-24"
             >
-            <div className="p-3 rounded-full">
+            <div className="p-3 rounded-full text-foreground">
                 {getIconForLink(link.name)}
             </div>
             <span className="text-sm font-medium truncate w-full text-center text-foreground">{link.name}</span>
